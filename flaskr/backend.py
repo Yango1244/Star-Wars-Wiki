2ae4f709d05fa7e10149101426804a1473f4e8e3
TadeoDeLaRocha <tadeo@techexchange.in>
2023-03-02 22:35:15 +0000

    Added Sign_up and Sign_in

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -1,12 +1,13 @@
 # TODO(Project 1): Implement Backend according to the requirements.
 from google.cloud import storage
+from hashlib import blake2s
 
 class Backend:
 
     def __init__(self):
-        self.content_bucket_name = 'fantasticwikicontent'
-        self.user_bucket_name = 'fantasticuserinfo'
         self.cur_client = storage.Client()
+        self.content_bucket = self.cur_client.get_bucket('fantasticwikicontent')
+        self.user_bucket = self.cur_client.get_bucket('fantasticuserinfo')
         
         
     def get_wiki_page(self, name):
@@ -21,11 +22,26 @@ class Backend:
     def upload(self):
         pass
 
-    def sign_up(self):
-        pass
+    def sign_up(self, username, password):
+        user_blob = self.user_bucket.get_blob(username)
 
-    def sign_in(self):
-        pass
+        if (user_blob):
+            return 
+        else:
+            user_blob = self.user_bucket.blob(username)
+            hashed_password = blake2s((password + username + "fantastic").encode('ASCII'))
+            user_blob.upload_from_string(hashed_password)
+
+    def sign_in(self, username, password):
+        user_blob = self.user_bucket.get_blob(username)
+
+        if (user_blob):
+            hashed_password = blake2s((password + username + "fantastic").encode('ASCII')).hexdigest()
+            with user_blob.open() as f:
+                correct_hash = f.read()
+                return correct_hash == hashed_password
+        else:
+            return False
 
     def get_image(self):
         pass
