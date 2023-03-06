99c43e3274974f3583dddc992c0665229d141a14
TadeoDeLaRocha <tadeo@techexchange.in>
2023-03-06 21:23:17 +0000

    Implemented incorrect sign up test

--- flaskr/backend_test.py
+++ flaskr/backend_test.py
@@ -13,15 +13,29 @@ def backend():
 
 
 @mock.patch('flaskr.backend.storage')
-def test_sign_up(mock_storage, backend):
+def test_sign_up_add_user(mock_storage, backend):
     mock_gcs_client = mock_storage.Client.return_value
     mock_bucket = Mock()
+    #Blob won't exist so we expect to make the blob
     mock_bucket.get_blob.return_value = None
     mock_gcs_client.get_bucket.return_value = mock_bucket
     backend = Backend()
     backend.sign_up("Capy", "CapybaraLove")
     mock_bucket.get_blob.assert_called_with('Capy')
     mock_bucket.blob.assert_called_once_with('Capy')
+
+@mock.patch('flaskr.backend.storage')
+def test_sign_up_user_exists(mock_storage, backend):
+    mock_gcs_client = mock_storage.Client.return_value
+    mock_bucket = Mock()
+    #Blob will exist so we expect to only call get_blob once
+    mock_bucket.get_blob.return_value = True
+    mock_gcs_client.get_bucket.return_value = mock_bucket
+
+    backend = Backend()
+    backend.sign_up("Capy", "CapybaraLove")
+    mock_bucket.get_blob.assert_called_once_with('Capy')
+    assert not mock_bucket.blob.called
 
 
 def test_sign_in(backend):
