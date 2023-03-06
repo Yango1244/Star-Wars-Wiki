4e2013f1176473b3a41d19c2b33db78bf05e2247
TadeoDeLaRocha <tadeo@techexchange.in>
2023-03-06 21:16:34 +0000

    Fixed backend pylint issues

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -1,13 +1,10 @@
-# TODO(Project 1): Implement Backend according to the requirements.
+"""Provides interface for google cloud storage buckets."""
 import os
-from google.cloud import storage
-from hashlib import blake2s
-import base64
-import io
-from flask import Flask, flash, request, redirect, url_for
-from werkzeug.utils import secure_filename
-import zipfile
 import glob
+import zipfile
+from hashlib import blake2s
+from google.cloud import storage
+from flask import Flask, request
 
 UPLOAD_FOLDER = './temp_files/'
 
@@ -15,30 +12,32 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 class Backend:
+    """Provides interface for google cloud storage buckets."""
     def __init__(self):
         self.cur_client = storage.Client()
         self.content_bucket_name = 'fantasticwikicontent'
         self.user_bucket_name = 'fantasticuserinfo'
         self.content_bucket = self.cur_client.get_bucket(self.content_bucket_name)
         self.user_bucket = self.cur_client.get_bucket(self.user_bucket_name)
-        
-        
+
     def get_wiki_page(self, name):
+        """Provides the page blob of a page from the name."""
         blobs = self.cur_client.list_blobs(self.content_bucket_name)
-        
+
         for item in blobs:
             if item.name == name:
                 return item
         return None
 
     def get_all_page_names(self):
-
+        """Provides a list of all wiki pages in the database"""
         blobs = self.cur_client.list_blobs(self.content_bucket_name)
         files = [blob.name for blob in blobs]
         return files
-        
-    
+
+
     def upload(self, file_name, file_obj):
+        """Uploads a file object to the database"""
         ALLOWED_EXTENSIONS = {'md', 'jpg', 'png', 'gif', 'jpeg'}
         wiki_name = request.form['wikiname']
 
@@ -68,15 +67,15 @@ class Backend:
                 if not allowed_file(file_name) or file_name == '':
                     all_allowed = False
 
-            if all_allowed != False:
+            if all_allowed is not False:
                 for file_name in os.listdir(UPLOAD_FOLDER):
                     blob = self.content_bucket.blob(wiki_name + '/' + file_name)
                     blob.upload_from_filename(UPLOAD_FOLDER + file_name)
 
                 clean_temp()
                 return "Success"
-            
-            elif all_allowed == False:
+
+            elif all_allowed is False:
                 clean_temp()
                 return "Failure"
 
@@ -93,16 +92,17 @@ class Backend:
             else:
                 clean_temp()
                 return "Failure"
-            
+
 
     def sign_up(self, username, password):
+        """Adds user data if it does not exist along with a hashed password."""
         #We first check if a blob already exists with that name
         user_blob = self.user_bucket.get_blob(username)
 
-        if (user_blob):
+        if user_blob:
             #If there is then we can't add the new user
             return False
-        
+
         user_blob = self.user_bucket.blob(username)
         #We create a blob with our hashed password
         hashed_password = blake2s((password + username + "fantastic").encode('ASCII'))
@@ -110,13 +110,14 @@ class Backend:
         return True
 
     def sign_in(self, username, password):
+        """Checks if a password, when hashed, matches the password in the user bucket."""
         #We first check if a blob already exists with that name
         user_blob = self.user_bucket.get_blob(username)
 
-        if (user_blob):
-            hashed_password = blake2s((password + username + "fantastic").encode('ASCII')).hexdigest()
-            with user_blob.open() as f:
-                correct_hash = f.read()
+        if user_blob:
+            hashed_password=blake2s((password + username + "fantastic").encode('ASCII')).hexdigest()
+            with user_blob.open() as file:
+                correct_hash = file.read()
                 #We compare the hash in the database with the hash of the password attempt
                 return correct_hash == hashed_password
         else:
@@ -124,6 +125,7 @@ class Backend:
             return False
 
     def get_image(self, name):
+        """Provides an image from the database"""
         # get image from gcs bucket
         # download to static folder
         # return url to be used in jinja template
@@ -131,5 +133,3 @@ class Backend:
         blob = bucket.blob(name)
         blob.download_to_filename("flaskr/static/" + name)
         return "../static/" + name
-
-
