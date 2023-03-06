57936ab7ced7d35be11c0ac381a3dc6aaa18c300
TadeoDeLaRocha <tadeo@techexchange.in>
2023-03-06 18:59:18 +0000

    Mocked backend for sign_up test

--- flaskr/backend_test.py
+++ flaskr/backend_test.py
@@ -1,4 +1,9 @@
 from flaskr.backend import Backend
+import unittest
+from unittest import mock
+from unittest.mock import Mock
+from unittest.mock import patch
+import google
 import pytest
 # TODO(Project 1): Write tests for Backend methods.
 
@@ -6,8 +11,18 @@ import pytest
 def backend():
     return Backend()
 
-def test_sign_up(backend):
+
+@mock.patch('flaskr.backend.storage')
+def test_sign_up(mock_storage, backend):
+    mock_gcs_client = mock_storage.Client.return_value
+    mock_bucket = Mock()
+    mock_bucket.get_blob.return_value = None
+    mock_gcs_client.get_bucket.return_value = mock_bucket
+    backend = Backend()
     backend.sign_up("Capy", "CapybaraLove")
+    mock_bucket.get_blob.assert_called_with('Capy')
+    mock_bucket.blob.assert_called_once_with('Capy')
+
 
 def test_sign_in(backend):
     assert backend.sign_in("Capy", "CapybaraLove")
